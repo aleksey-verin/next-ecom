f1d54f621fd3cf494154e28eb7cc35732ef0f677
Aleksey Verin <verevaa@gmail.com>
2024-09-04 18:24:16 +0300

    feat: add local images

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -60,7 +60,7 @@ async function up() {
     data: {
       name: 'Пепперони фреш',
       imageUrl:
-        'https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp',
+        '/images/35.webp',
       categoryId: 1,
       ingredients: {
         connect: _ingredients.slice(0, 5),
@@ -72,7 +72,7 @@ async function up() {
     data: {
       name: 'Сырная',
       imageUrl:
-        'https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp',
+      '/images/36.webp',
       categoryId: 1,
       ingredients: {
         connect: _ingredients.slice(5, 10),
@@ -84,7 +84,7 @@ async function up() {
     data: {
       name: 'Чоризо фреш',
       imageUrl:
-        'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp',
+      '/images/37.webp',
       categoryId: 1,
       ingredients: {
         connect: _ingredients.slice(10, 40),
@@ -163,27 +163,27 @@ async function up() {
   //   data: [
   //     {
   //       previewImageUrl:
-  //         'https://cdn.inappstory.ru/story/xep/xzh/zmc/cr4gcw0aselwvf628pbmj3j/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=3101815496',
+  //         '/images/38.webp',
   //     },
   //     {
   //       previewImageUrl:
-  //         'https://cdn.inappstory.ru/story/km2/9gf/jrn/sb7ls1yj9fe5bwvuwgym73e/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=3074015640',
+  //        '/images/39.webp',
   //     },
   //     {
   //       previewImageUrl:
-  //         'https://cdn.inappstory.ru/story/quw/acz/zf5/zu37vankpngyccqvgzbohj1/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=1336215020',
+  //         '/images/40.webp',
   //     },
   //     {
   //       previewImageUrl:
-  //         'https://cdn.inappstory.ru/story/7oc/5nf/ipn/oznceu2ywv82tdlnpwriyrq/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=38903958',
+  //         '/images/41.webp',
   //     },
   //     {
   //       previewImageUrl:
-  //         'https://cdn.inappstory.ru/story/q0t/flg/0ph/xt67uw7kgqe9bag7spwkkyw/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=2941222737',
+  //        '/images/42.webp',
   //     },
   //     {
   //       previewImageUrl:
-  //         'https://cdn.inappstory.ru/story/lza/rsp/2gc/xrar8zdspl4saq4uajmso38/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=4207486284',
+  //        '/images/43.webp',
   //     },
   //   ],
   // });
@@ -193,27 +193,27 @@ async function up() {
   //     {
   //       storyId: 1,
   //       sourceUrl:
-  //         'https://cdn.inappstory.ru/file/dd/yj/sx/oqx9feuljibke3mknab7ilb35t.webp?k=IgAAAAAAAAAE',
+  //         '/images/44.webp',
   //     },
   //     {
   //       storyId: 1,
   //       sourceUrl:
-  //         'https://cdn.inappstory.ru/file/jv/sb/fh/io7c5zarojdm7eus0trn7czdet.webp?k=IgAAAAAAAAAE',
+  //        '/images/45.webp',
   //     },
   //     {
   //       storyId: 1,
   //       sourceUrl:
-  //         'https://cdn.inappstory.ru/file/ts/p9/vq/zktyxdxnjqbzufonxd8ffk44cb.webp?k=IgAAAAAAAAAE',
+  //        '/images/46.webp',
   //     },
   //     {
   //       storyId: 1,
   //       sourceUrl:
-  //         'https://cdn.inappstory.ru/file/ur/uq/le/9ufzwtpdjeekidqq04alfnxvu2.webp?k=IgAAAAAAAAAE',
+  //         '/images/47.webp',
   //     },
   //     {
   //       storyId: 1,
   //       sourceUrl:
-  //         'https://cdn.inappstory.ru/file/sy/vl/c7/uyqzmdojadcbw7o0a35ojxlcul.webp?k=IgAAAAAAAAAE',
+  //        '/images/48.webp',
   //     },
   //   ],
   // });
